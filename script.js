568f3a0b4217f90566734efe4d2ee71afdbeb44b
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-01-18 17:37:48 +0000

    day 4

--- script.js
+++ script.js
@@ -5,7 +5,29 @@ password = document.querySelector("#password")
 password2 = document.querySelector("#password2")
 
 
-naame.addEventListener('mouseout', function(){
+naame.addEventListener('focusout', function(){
     password2.setAttribute('placeholder',`...or xX${naame.value}123`)
 })
-password2
+
+password2.addEventListener('focusout', function(){
+    if(password2.value != password.value){
+        password2.setAttribute('style', "border: solid 1px red;")
+        
+        if(password2.parentNode.lastChild.innerText !== "passwords don't match") {
+            let err = document.createElement('p')
+            err.innerText = "passwords don't match"
+            err.style = "font-size: 0.7vw; color: red;"
+            password2.parentNode.appendChild(err)
+        }         
+    } else {
+        if(password2.parentNode.lastChild.innerText === "passwords don't match") {
+            let err = password2.parentNode.lastChild
+            password2.parentNode.removeChild(err)
+        }
+
+        password2.style = "border: 1px solid green;"
+        
+    }
+
+})
+
